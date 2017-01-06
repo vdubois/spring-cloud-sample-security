5b2448f375adfd3062f7deacd3647c74a1e23229
Vincent Dubois <dubois.vct@free.fr>
2017-01-06 10:59:14 +0100

    [FEATURE] Adding bean validation to authentication

--- src/main/java/io/github/vdubois/model/AuthenticationDTO.java
+++ src/main/java/io/github/vdubois/model/AuthenticationDTO.java
@@ -1,12 +1,17 @@
 package io.github.vdubois.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
 
 /**
  * Created by vdubois on 31/12/16.
  */
 @Data
 public class AuthenticationDTO {
+
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String password;
 }
